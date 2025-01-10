a9d7ac8d643c31efd057c245fbff0b3a2f85886d
Mateus Milczewski <mateus.jmaf@gmail.com>
2025-01-10 05:42:37 -0300

    changed base name to new repository name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://mateusjmaf.github.io',
-  base: '/matx',
+  base: '/design',
 });
